c7db3dd2b0681456d1eea8cf7e896557241d7ef5
zayedrayed177-cmd <zayedrayed177@gmail.com>
2026-01-22 22:12:48 +0300

    Add new token configuration for additional server

--- tokens.js
+++ tokens.js
@@ -15,4 +15,19 @@ export default [
         },
         selfMute: true,
     },
+    {
+        channelId: "1299121795621720104",
+        serverId: "1281864747255468104",
+        token: process.env.token2,
+        selfDeaf: false,
+        autoReconnect: {
+            enabled: true,
+            delay: 5, // ثواني
+            maxRetries: 5,
+        },
+        presence: {
+            status: "online",
+        },
+        selfMute: true,
+    },
 ];
