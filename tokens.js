60526a23da2f88298f30645c8382b9602fd83312
zayedrayed177-cmd <zayedrayed177@gmail.com>
2026-01-22 22:11:12 +0300

    Update channelId, serverId, and presence status

--- tokens.js
+++ tokens.js
@@ -1,8 +1,8 @@
 // status can be "online", "idle", "dnd", or "invisible" or "offline"
 export default [
     {
-        channelId: "1344059457747026005",
-        serverId: "1344059457046319198",
+        channelId: "1299121795621720104",
+        serverId: "1281864747255468104",
         token: process.env.token1,
         selfDeaf: false,
         autoReconnect: {
@@ -11,7 +11,7 @@ export default [
             maxRetries: 5,
         },
         presence: {
-            status: "idle",
+            status: "online",
         },
         selfMute: true,
     },
