6abeba08fd934c8d1be7b9c50057133a6dccb3be
zayedrayed177-cmd <zayedrayed177@gmail.com>
2026-02-05 13:29:45 +0300

    Update tokens.js

--- tokens.js
+++ tokens.js
@@ -31,49 +31,5 @@ export default [
         selfMute: true,
     },
     {
-        channelId: "1425863585934082211",
-        serverId: "1374408045874122833",
-        token: process.env.token4,
-        selfDeaf: false,
-        autoReconnect: {
-            enabled: true,
-            delay: 5, // ثواني
-            maxRetries: 5,
-        },
-        presence: {
-            status: "online",
-        },
-        selfMute: true,
-    },
-    {
-        channelId: "1417230249913090059",
-        serverId: "965765353571504138",
-        token: process.env.token3,
-        selfDeaf: false,
-        autoReconnect: {
-            enabled: true,
-            delay: 5, // ثواني
-            maxRetries: 5,
-        },
-        presence: {
-            status: "online",
-        },
-        selfMute: true,
-    },
-    {
-        channelId: "1455243294694179091",
-        serverId: "1363439136756727951",
-        token: process.env.token4,
-        selfDeaf: true,
-        autoReconnect: {
-            enabled: true,
-            delay: 5, // ثواني
-            maxRetries: 5,
-        },
-        presence: {
-            status: "online",
-        },
-        selfMute: true,
-    },
 ];
 
